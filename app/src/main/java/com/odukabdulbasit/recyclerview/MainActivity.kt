686eaeca1d086c3ea70bcface3515a7c42e35f1f
Abdulbasit Oduk <a.oduk@pegb.tech>
2023-08-14 11:50:05 +0300

    Connected Adapter with the list and created items and connected the ui with the code

--- app/src/main/java/com/odukabdulbasit/recyclerview/MainActivity.kt
+++ app/src/main/java/com/odukabdulbasit/recyclerview/MainActivity.kt
@@ -2,10 +2,32 @@ package com.odukabdulbasit.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.odukabdulbasit.data.ItemModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var itemAdapter: ItemAdapter
+    private val itemList = mutableListOf<ItemModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Create sample data
+        itemList.add(ItemModel("Item 1"))
+        itemList.add(ItemModel("Item 2"))
+        itemList.add(ItemModel("Item 3"))
+        itemList.add(ItemModel("Item 4"))
+        itemList.add(ItemModel("Item 5"))
+        itemList.add(ItemModel("Item 6"))
+
+        itemAdapter = ItemAdapter(itemList)
+        recyclerView.adapter = itemAdapter
     }
 }
