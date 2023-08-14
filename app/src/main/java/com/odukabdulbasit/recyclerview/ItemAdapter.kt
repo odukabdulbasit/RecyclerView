facd89b189cea36711aeed73eab60d10dda1c62a
Abdulbasit Oduk <a.oduk@pegb.tech>
2023-08-14 11:28:40 +0300

    Created RecyclerView Adapter

---
+++ app/src/main/java/com/odukabdulbasit/recyclerview/ItemAdapter.kt
@@ -0,0 +1,27 @@
+package com.odukabdulbasit.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.odukabdulbasit.data.ItemModel
+
+class ItemAdapter(private val itemList: List<ItemModel>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return ItemViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.itemNameTextView.text = item.itemName
+    }
+
+    override fun getItemCount(): Int = itemList.size
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val itemNameTextView: TextView = itemView.findViewById(R.id.itemNameTextView)
+    }
+}
